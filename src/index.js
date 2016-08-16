d4f3c2787d55367e7d2477e94c310d1277293f3c
Timothy Farrell <tim@thecookiejar.me>
2016-08-16 08:39:15 -0500

    Expose supporting things also to support more flexible screens.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-module.exports = require('./deprecated/indexDeprecated');
+import Navigation from './Navigation';
+import PropRegistry from './PropRegistry';
+import Screen from './Screen';
+import {NavigationToolBarIOS} from 'react-native-controllers';
+
+module.exports = {
+  Navigation,
+  NavigationToolBarIOS,
+  PropRegistry,
+  Screen
+};
